b802d777b3270cf7885087ea4358c1d64b5011a5
FanilBiktimirov <70664039+FanilBiktimirov@users.noreply.github.com>
2020-09-19 18:35:51 +0500

    Update Main.java

--- ConsoleCalculator/src/Main.java
+++ ConsoleCalculator/src/Main.java
@@ -8,12 +8,12 @@ public class Main {
         System.out.println("Внимание! Калькулятор принимает числа от 1 до 10 включительно.");
         System.out.println ("Калькулятор умеет работать только с целыми числами, только с арабскими или римскими цифрами одновременно");
         System.out.println("При вводе неподходящих чисел приложение завершает свою работу.");
-        System.out.println("Введите данные для операции через пробел:");
+        System.out.println("Введите данные через пробел:");
         try (Scanner sc = new Scanner(System.in)) {
             String result = new Calc().calculate(sc.nextLine().trim().split(" "));
             System.out.println("Ответ: " + result);
         } catch (Exception e) {
-            System.out.println("Ошибка! Введены некорректные данные!");
+            System.out.println("Ошибка! Данные введены некорректно!");
         }
 
     }
